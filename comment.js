e654ad3477b45686f5e0baeca268a2aa0cba8a96
e5science <139855049+e5science@users.noreply.github.com>
2023-07-19 19:57:16 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,6 +1,30 @@
 // Create web server
+// Node.js
+// 2018-02-23
+// --------------------------------------
 
-// Import module
-const express = require('express');
-const router = express.Router();
-const commentController = require('../controllers/comment');
+// Imports
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+// Server
+http.createServer(function (req, res) {
+    // Get the path
+    var path = url.parse(req.url).pathname;
+    // Get the file
+    fs.readFile(__dirname + path, function (err, data) {
+        if (err) {
+            // Error
+            res.writeHead(404);
+            res.end("Error: 404 - Page not found");
+            return;
+        }
+        // Else
+        res.writeHead(200);
+        res.end(data);
+    });
+}).listen(8080);
+
+// Log
+console.log("Server running at http://localhost:8080/");
